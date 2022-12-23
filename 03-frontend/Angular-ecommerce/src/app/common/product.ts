dfc59dbd6e7f041ee8820713deee564afef3d7af
AAliasghar <a.asgharinia@yahoo.com>
2022-12-24 00:21:03 +0100

    Product Detail

--- 03-frontend/Angular-ecommerce/src/app/common/product.ts
+++ 03-frontend/Angular-ecommerce/src/app/common/product.ts
@@ -1,14 +1,15 @@
 export class Product {
         // Properties are delared on constructor and match with JSON from Spring Boot
-    constructor(public sku: string,
-                public name: string,
-                public description: string,
-                public unitPrice: number,
-                public imageUrl: string,
-                public active: boolean,
-                public unitsInStock: number,
-                public dateCreated: Date,
-                public lastUpdated: Date
-        ) {
-    }
+        constructor(public id?: string,
+            public sku?: string,   // ""?" is safe  navigator operator -> if products Null/Undiefined
+            public name?: string,
+            public description?: string,
+            public unitPrice?: number,
+            public imageUrl?: string,
+            public active?: boolean,
+            public unitsInStock?: number,
+            public dateCreated?: Date,
+            public lastUpdated?: Date
+    ) {
+}
 }
